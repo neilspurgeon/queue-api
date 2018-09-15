afdfeb588ba3c662f557e51a4db6de55573e751a
Neil <neilspurgeon@gmail.com>
2018-09-15 15:11:10 -0700

    init

---
+++ app/controllers/examples_controller.rb
@@ -0,0 +1,9 @@
+class ExamplesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    examples = Example.all.select(:id, :name, :color)
+    render json: examples
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+# class SessionsController < Devise::SessionsController
+#   respond_to :json
+
+#   private
+
+#   def respond_with(resource, _opts = {})
+#     render json: resource
+#   end
+
+#   def respond_to_on_destroy
+#     head :no_content
+#   end
+# end
+
+class SessionsController < Devise::SessionsController
+  def create
+    print current_token
+    super { @token = current_token }
+  end
+
+  def show
+  end
+
+  private
+
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
+end
