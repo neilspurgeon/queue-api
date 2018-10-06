9d72cbba493271b8c9441b44e8254b0d9eed2a6c
Neil <neilspurgeon@gmail.com>
2018-10-05 20:34:25 -0700

    broadcasts back song change

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -1,20 +1,17 @@
 class RoomsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "rooms_channel"
-    p 'subscribed to Rooms Channel ~~~~~~ YAYA'
+    p '~~ Subscribed to Rooms Channel ~~'
   end
 
   def track_change(data)
-    p 'track changed'
-    # p params
-    p data
-    p '–––––––––––––––––––––––––––––––––––––––––––––––'
-    # @track = params[:track]
-    ActionCable.server.broadcast('rooms_channel', {
+    payload = {
       'trackChanged': {
         'track': data
       }
-    })
+    }
+
+    ActionCable.server.broadcast('rooms_channel', payload )
   end
 
   def unsubscribed
