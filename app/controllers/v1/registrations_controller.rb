688a2bd09ce9f03ad12b21f4f77fa01f6d9d3c2d
Neil <neilspurgeon@gmail.com>
2019-01-24 18:02:06 -0800

    allow editing profile info without password

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -8,6 +8,10 @@ class V1::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_password)
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
