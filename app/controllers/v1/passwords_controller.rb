80f3a5bf18ab884da6ad84cd40ad02aa78f76c58
Neil <neilspurgeon@gmail.com>
2019-01-25 21:16:03 -0800

    allow users to update password

--- app/controllers/v1/passwords_controller.rb
+++ app/controllers/v1/passwords_controller.rb
@@ -1,3 +1,30 @@
 class V1::PasswordsController < Devise::PasswordsController
 
+  before_action :authenticate_user!
+
+  def update
+    current_password = password_update_params[:current_password]
+    new_password = password_update_params[:new_password]
+
+    if !current_user.valid_password?(current_password)
+     return render :json => {'error': 'Your current password is incorrect'}
+    end
+
+    current_user.password = new_password
+
+    if !current_user.valid?
+      return render :json => {'error': 'Your new password is not valid'}
+    end
+
+    current_user.save
+    return render :json => {'success': 'Your password was successfully updated'}
+
+  end
+
+  private
+
+  def password_update_params
+    params.require(:user).permit(:new_password, :current_password)
+  end
+
 end
